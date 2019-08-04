1e9e3323af495d27da300bfb8e235c769d4de9ab
ruxm <50956006+ruxm@users.noreply.github.com>
2019-08-04 19:00:55 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+
+public class Main
+{
+  public static void main(string[] args) throws IOExpention
+  {
+    ServerSocket ss = new ServerSocket(4999);
+    Socket s = ss.accept();
+  }
+}
